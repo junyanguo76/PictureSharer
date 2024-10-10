031c20f7906fb81f753eb3f9423420dfe2c41390
Junyan <junyanguo76@gmail.com>
2024-10-11 05:11:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/picturesharer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.picturesharer;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
